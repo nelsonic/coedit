51c9f5f72a4216a7d6f20e34238bd052c4fdd464
@nelsonic <nodecoder@gmail.com>
2013-12-08 09:14:30 +0000

    changes the way test run counter works from a number to an array of timestamps - works consistently

--- test/fs.js
+++ test/fs.js
@@ -1,5 +1,6 @@
-var assert = require('assert'); 
-var fs     = require('fs');
+var assert  = require('assert'); 
+var fs      = require('fs');
+var lastRun = 
 
 describe('Node.js Environment Checks', function(){
   describe('Basic IO', function(){
@@ -11,39 +12,33 @@ describe('Node.js Environment Checks', function(){
         });
     })
 
-    it('READ ./test/runCount.txt & runCount is number', function(){
-        fs.readFile('./test/runCount.txt', 'utf8', function read(err, data) {
-            if (err) {
-                throw err;
-            }
+    it('READ from a file', function(){
+        fs.readFile('./test/testruns.txt', 'utf8', function(err, data) {
+            if (err) console.log(err);
             // console.log(data);
-            var runCount = parseInt(data,10)
-            assert( typeof runCount === 'number');
+            var runCount = String(data).split("\n");
+            // console.log('\n --- '+data +' --- \n')
+            assert(runCount.length > 0);
         });
     })
 
-    xit('WRITE to (increment) ./test/runCount.txt', function(){
-        var runCountFile = "./test/runCount.txt";
-        fs.readFile(runCountFile, 'utf8', function read(err, data) {
-            if (err) {
-                throw err;
-            }
-            var runCount = parseInt(data,10);
-            // increment the runCount
-            var newRunCount = runCount + 1;
-            fs.appendFile(runCountFile, ","+newRunCount, function(err) {
+    it('WRITE to a file', function(done){
+        // var CWD = process.cwd();
+        var file = './test/testruns.txt';
+        fs.readFile(file, 'utf8', function(err, data) {
+            if (err) console.log(err);
+            var before = String(data).split("\n");
+            var time = String(new Date().getTime());
+            // console.log("Time : "+time)
+            fs.appendFile(file, '\n'+time, function(err) {
                 if (err) console.log(err);
-                // if(err) {
-                //     console.log(err);
-                // } else {
-                    fs.readFile(runCountFile, 'utf8', function read(err, data) {
-                        
-                        var runCountIncremented = parseInt(data,10);
-                        // console.log("Run Count Updated: "+runCount +' | '+runCountIncremented);
-                        // confirm we have incremented the runCount:
-                        assert(runCountIncremented === runCount + 1);
-                    }); // end inner fs.readFile
-                // } // end else
+                fs.readFile(file, 'utf8', function(err, data) {
+                    if (err) console.log(err);
+
+                    var after = String(data).split("\n");
+                    assert(after.length === before.length+1);
+                    done();
+                }); // end inner fs.readFile
             }); // end fs.writeFile
         }); // end fs.readFile
     })
@@ -53,19 +48,18 @@ describe('Node.js Environment Checks', function(){
         var CWD = process.cwd(); //__filename.substring(0, __filename.lastIndexOf('/'));
         // console.log(CWD);
         var newFile = new Date().getTime() +".txt",
-            newFilePath = CWD+'/tmp/'+newFile;
+            newFilePath = CWD+'/test/'+newFile;
         fs.writeFile(newFilePath, "hello!", function (err) {
             if (err) console.log(err);
-            // console.log("Created file: "+newFile);
-            fs.readdir(CWD+'/tmp/', function(err, list) {
+            // confirm the new file is in the directory
+            fs.readdir(CWD+'/test/', function(err, list) {
                 // console.log(list)
-                assert(list.indexOf(newFile) > -1)
-
-                // process.nextTick( function() {
-                //     fs.unlink(newFilePath, function(err, data) {
-                //         if (err) throw err;
-                //     });
-                // });
+                assert(list.indexOf(newFile) > -1);
+                process.nextTick( function() {
+                    fs.unlink(newFilePath, function(err, data) {
+                        if (err) throw err;
+                    });
+                });
                 done()
             });
         });
